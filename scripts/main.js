a36b4388d3f6edbec372dfcee22268b49eda1797
dannycrews <dancrews@me.com>
2016-08-16 16:30:34 -0400

    Add state for fish

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,21 @@ var h = require('./helpers');
 */
 
 var App = React.createClass({
+  getInitialState : function() {
+    return {
+      fishes : {},
+      order : {}
+    }
+  },
+
+  addFish : function(fish) {
+    var timestamp = (new Date()).getTime();
+    // update the state object
+    this.state.fishes['fish-' + timestamp] = fish;
+    // set the state
+    this.setState({ fishes : this.state.fishes });
+  },
+
 
   render : function() {
     return (
@@ -23,7 +38,7 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
       )
   }
@@ -35,6 +50,7 @@ var App = React.createClass({
 */
 
 var Header = React.createClass({
+
   render : function() {
     return (
       <header className="top">
@@ -71,10 +87,53 @@ var Header = React.createClass({
  var Inventory = React.createClass({
    render : function() {
      return (
-       <p>Inventory</p>
+       <div>
+         <h2>Inventory</h2>
+
+         <AddFishForm {...this.props} />
+       </div>
        )
    }
  });
+
+ /*
+  Add fish form
+  */
+
+var AddFishForm = React.createClass({
+  createFish : function(event) {
+    // 1. stop the form from submitting
+    event.preventDefault();
+    // 2. take the data from the form and create an object
+     var fish = {
+      name : this.refs.name.value,
+      price : this.refs.price.value,
+      status : this.refs.status.value,
+      description : this.refs.description.value,
+      image : this.refs.image.value
+     }
+    // 3. add the fish to the App state
+    this.props.addFish(fish);
+    this.ref.fishForm.reset();
+  },
+
+  render : function() {
+    return (
+      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+        <input type="text" ref="name" placeholder="Fish Name"/>
+        <input type="text" ref="price" placeholder="Fish Price" />
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="URL to Image" />
+        <button type="submit">+ Add Item </button>
+      </form>
+      )
+
+  }
+});
 
  /*
   StorePicker
