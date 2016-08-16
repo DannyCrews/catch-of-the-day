7e9b7708fc0b1b1fbb19f733bc9c1a634ef7a26c
dannycrews <dancrews@me.com>
2016-08-16 11:10:34 -0400

    Set up app html structure.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,64 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+  App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+      )
+  }
+});
+
+/*
+  App
+    Header
+*/
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+      )
+  }
+});
+
+ /*
+   App
+    Order
+ */
+
+ var Order = React.createClass({
+   render : function() {
+     return (
+       <p>Order</p>
+       )
+   }
+ });
+
+ /*
+   App
+    Inventory
+ */
+
+ var Inventory = React.createClass({
+   render : function() {
+     return (
+       <p>Inventory</p>
+       )
+   }
+ });
+
+ /*
   StorePicker
   This will let us make <StorePicker />
  */
@@ -19,4 +77,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
