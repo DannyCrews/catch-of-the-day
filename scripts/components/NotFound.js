772044481bee04c18a361b0b3d34cbbcca977eb7
dannycrews <dancrews@me.com>
2016-08-24 21:35:09 -0400

    Refactor to classes and use autobind decorator

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -1,13 +1,12 @@
 /*
   Not Found
  */
-
 import React from 'react';
 
-var NotFound = React.createClass({
-  render : function() {
+class NotFound extends React.Component {
+  render() {
     return <h1>Not Found</h1>
   }
-});
+}
 
 export default NotFound;
--- scripts/components/StorePicker.js
+++ scripts/components/StorePicker.js
@@ -5,27 +5,31 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [History],
+@autobind
+class StorePicker extends React.Component {
 
-  goToStore : function(event) {
-    event.preventDefault();
-    // get the data from the input
-    var storeId = this.refs.storeId.value;
-    this.history.pushState(null, '/store/' + storeId);
-    // transition from <StorePicker/> to <App/>
-  },
+   goToStore(event) {
+     event.preventDefault();
+     // get the data from the input
+     var storeId = this.refs.storeId.value;
+     this.history.pushState(null, '/store/' + storeId);
+     // transition from <StorePicker/> to <App/>
+   }
 
-  render : function() {
-    return (
-      <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter a Store</h2>
-        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
-        <input type="submit" />
-      </form>
+   render() {
+     return (
+       <form className="store-selector" onSubmit={this.goToStore}>
+         <h2>Please Enter a Store</h2>
+         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+         <input type="submit" />
+       </form>
       )
-  }
-});
+   }
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
